153a6338039913eea0efe08db8fa4a8fed9ae4a3
buuchuthe <buuchuthe@gmail.com>
2026-03-10 19:57:51 +0700

    Update Pneumatic Fittings

--- assets/js/pneumatic_fittings.js
+++ assets/js/pneumatic_fittings.js
@@ -1,11 +1,35 @@
 const productMap = {
   "Đầu nối nhanh khí nén (One touch fitting)": {
-    "Đầu Nối Nhanh Khí Nén Thẳng - Có Ren": {
+    "Tube Metric": {
+      "Bulkhead Union": {
+        dim: "PMM Series.jpg",
+        eng: "Bulkhead Union",
+        thumb: "https://cdcpneumatics.com/data/product/pmm.png",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>T</th><th>L</th><th>E</th><th>A</th><th>B</th><th>H(Hex)</th></tr></thead><tbody><tr><td>PMM 04</td><td>4</td><td>M12*1.0p</td><td>35.5</td><td>4</td><td>15.5</td><td>14.8</td><td>14</td></tr><tr><td>PMM 06</td><td>6</td><td>M14*1.0p</td><td>39.8</td><td>4</td><td>18.2</td><td>16.4</td><td>17</td></tr><tr><td>PMM 08</td><td>8</td><td>M16*1.0p</td><td>42.2</td><td>5</td><td>18.3</td><td>18.4</td><td>19</td></tr><tr><td>PMM 10</td><td>10</td><td>M20*1.0p</td><td>45.1</td><td>5</td><td>19.2</td><td>19.7</td><td>24</td></tr><tr><td>PMM 12</td><td>12</td><td>M22*1.0p</td><td>50.2</td><td>5</td><td>24.1</td><td>21.9</td><td>26</td></tr><tr><td>PMM 16</td><td>16</td><td>M27*1.0p</td><td>53.1</td><td>6</td><td>22.8</td><td>24.0</td><td>32</td></tr></tbody></table>`,
+      },
+      "Plastic Bulkhead Union": {
+        dim: "PMP Series.jpg",
+        eng: "Plastic Bulkhead Union",
+        thumb: "https://cdcpneumatics.com/data/product/1680845450_image1.jpg",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>T</th><th>L</th><th>E</th><th>A</th><th>B</th><th>H1(Hex)</th><th>H2(Hex)</th></tr></thead><tbody><tr><td>PMP 04</td><td>4</td><td>M12*1.25p</td><td>32.0</td><td>6</td><td>6.3</td><td>14.8</td><td>13</td><td>16</td></tr><tr><td>PMP 06</td><td>6</td><td>M15*1.5p</td><td>36.0</td><td>6.5</td><td>7.7</td><td>16.4</td><td>15</td><td>18</td></tr><tr><td>PMP 08</td><td>8</td><td>M17*1.5p</td><td>38.8</td><td>7.0</td><td>8.4</td><td>18.3</td><td>17</td><td>21</td></tr><tr><td>PMP 10</td><td>10</td><td>M20*1.75p</td><td>44.1</td><td>8.0</td><td>10.5</td><td>20.2</td><td>21</td><td>25</td></tr><tr><td>PMP 12</td><td>12</td><td>M24*1.75p</td><td>47.6</td><td>8.5</td><td>11.0</td><td>22.9</td><td>24</td><td>28</td></tr></tbody></table>`,
+      },
+    },
+    "Tube Metric - Thread R (or Rc)": {
       "Male Straight": {
         dim: "PC Series.jpg",
         eng: "Male Straight",
         thumb: "https://cdcpneumatics.com/data/product/pc.png",
-        desc: `Tube Metric - Thread R<br>
+        desc: `
         <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
         <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
         <b>Working Temperature Range</b>: 32~140℉, 0~60℃
@@ -16,7 +40,7 @@ const productMap = {
         dim: "PCF Series.jpg",
         eng: "Female Straight",
         thumb: "https://cdcpneumatics.com/data/product/pcf.png",
-        desc: `Tube Metric - Thread Rc<br>
+        desc: `
         <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
         <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
         <b>Working Temperature Range</b>: 32~140℉, 0~60℃
@@ -27,7 +51,7 @@ const productMap = {
         dim: "POC Series.jpg",
         eng: "Round Male Straight",
         thumb: "https://cdcpneumatics.com/data/product/poc.png",
-        desc: `Tube Metric - Thread R<br>
+        desc: `
         <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
         <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
         <b>Working Temperature Range</b>: 32~140℉, 0~60℃
@@ -38,24 +62,71 @@ const productMap = {
         dim: "PCP Series.jpg",
         eng: "Coupler Plug",
         thumb: "https://cdcpneumatics.com/data/product/pcp.png",
-        desc: `Tube Metric<br>
+        desc: `
         <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
         <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
         <b>Working Temperature Range</b>: 32~140℉, 0~60℃
         `,
         table: `<table><thead><tr><th>Model</th><th>∅D</th><th>∅P</th><th>￠A</th><th>L</th><th>E</th><th>B</th></tr></thead><tbody><tr><td>PCP 04</td><td>4</td><td>10.0</td><td>10.9</td><td>31.0</td><td>16.3</td><td>14.8</td></tr><tr><td>PCP 06</td><td>6</td><td>11.9</td><td>10.9</td><td>31.7</td><td>16.3</td><td>17.4</td></tr><tr><td>PCP 08</td><td>8</td><td>14.0</td><td>10.9</td><td>34.9</td><td>16.3</td><td>18.4</td></tr><tr><td>PCP 10</td><td>10</td><td>16.0</td><td>10.9</td><td>42.4</td><td>16.3</td><td>19.7</td></tr><tr><td>PCP 12</td><td>12</td><td>19.0</td><td>10.9</td><td>45.4</td><td>16.3</td><td>21.9</td></tr><tr><td>PCP 16</td><td>16</td><td>24.0</td><td>10.9</td><td>49.3</td><td>16.3</td><td>24.5</td></tr></tbody></table>`,
+      }, 
+      "Bulkhead Female Straight": {
+        dim: "BMF Series.jpg",
+        eng: "Bulkhead Female Straight",
+        thumb: "https://cdcpneumatics.com/data/product/1772182616_image1_eng.png",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>∅P</th><th>T1</th><th>T2</th><th>L</th><th>E</th><th>A1</th><th>A2</th><th>B</th><th>H1(Hex)</th><th>H2(Hex)</th></tr></thead><tbody><tr><td>PMF04-01</td><td>4</td><td>10.0</td><td>R 1/8</td><td>M12*1.0p</td><td>27.1</td><td>4</td><td>9.0</td><td>10.6</td><td>14.8</td><td>14</td><td>14</td></tr><tr><td>PMF04-02</td><td>4</td><td>10.0</td><td>R 1/4</td><td>M12*1.0p</td><td>29.1</td><td>4</td><td>11.0</td><td>10.6</td><td>14.8</td><td>17</td><td>14</td></tr><tr><td>PMF04-03</td><td>4</td><td>10.0</td><td>R 3/8</td><td>M12*1.0p</td><td>30.1</td><td>4</td><td>12.0</td><td>10.6</td><td>14.8</td><td>22</td><td>14</td></tr><tr><td>PMF06-01</td><td>6</td><td>12.0</td><td>R 1/8</td><td>M14*1.0p</td><td>30.2</td><td>4</td><td>9.0</td><td>11.0</td><td>16.4</td><td>17</td><td>17</td></tr><tr><td>PMF06-02</td><td>6</td><td>12.0</td><td>R 1/4</td><td>M14*1.0p</td><td>30.2</td><td>4</td><td>11.0</td><td>11.0</td><td>16.4</td><td>17</td><td>17</td></tr><tr><td>PMF06-03</td><td>6</td><td>12.0</td><td>R 3/8</td><td>M14*1.0p</td><td>31.2</td><td>4</td><td>12.0</td><td>11.0</td><td>16.4</td><td>22</td><td>17</td></tr><tr><td>PMF08-01</td><td>8</td><td>14.0</td><td>R 1/8</td><td>M16*1.0p</td><td>31.4</td><td>5</td><td>9.0</td><td>12.0</td><td>18.4</td><td>19</td><td>19</td></tr><tr><td>PMF08-02</td><td>8</td><td>14.0</td><td>R 1/4</td><td>M16*1.0p</td><td>31.4</td><td>5</td><td>11.0</td><td>12.0</td><td>18.4</td><td>19</td><td>19</td></tr><tr><td>PMF08-03</td><td>8</td><td>14.0</td><td>R 3/8</td><td>M16*1.0p</td><td>32.4</td><td>5</td><td>12.0</td><td>12.0</td><td>18.4</td><td>22</td><td>19</td></tr><tr><td>PMF08-04</td><td>8</td><td>14.0</td><td>R 1/2</td><td>M16*1.0p</td><td>34.4</td><td>5</td><td>14.0</td><td>12.0</td><td>18.4</td><td>24</td><td>19</td></tr><tr><td>PMF10-01</td><td>10</td><td>16.1</td><td>R 1/8</td><td>M20*1.0p</td><td>33.6</td><td>5</td><td>9.0</td><td>12.7</td><td>19.7</td><td>24</td><td>24</td></tr><tr><td>PMF10-02</td><td>10</td><td>16.1</td><td>R 1/4</td><td>M20*1.0p</td><td>33.6</td><td>5</td><td>11.0</td><td>12.7</td><td>19.7</td><td>24</td><td>24</td></tr><tr><td>PMF10-03</td><td>10</td><td>16.1</td><td>R 3/8</td><td>M20*1.0p</td><td>33.6</td><td>5</td><td>12.0</td><td>12.7</td><td>19.7</td><td>24</td><td>24</td></tr><tr><td>PMF10-04</td><td>10</td><td>16.1</td><td>R 1/2</td><td>M20*1.0p</td><td>35.6</td><td>5</td><td>14.0</td><td>12.7</td><td>19.7</td><td>24</td><td>24</td></tr><tr><td>PMF12-02</td><td>12</td><td>19.0</td><td>R 1/4</td><td>M22*1.0p</td><td>38.3</td><td>5</td><td>11.0</td><td>17.1</td><td>21.9</td><td>24</td><td>26</td></tr><tr><td>PMF12-03</td><td>12</td><td>19.0</td><td>R 3/8</td><td>M22*1.0p</td><td>38.3</td><td>5</td><td>12.0</td><td>17.1</td><td>21.9</td><td>24</td><td>26</td></tr><tr><td>PMF12-04</td><td>12</td><td>19.0</td><td>R 1/2</td><td>M22*1.0p</td><td>40.3</td><td>5</td><td>14.0</td><td>17.1</td><td>21.9</td><td>24</td><td>26</td></tr></tbody></table>`,
       },
-      
       "Blank": {
         dim: "Blank Series.jpg",
         eng: "Blank",
         thumb: "",
-        desc: `Tube Metric - Thread R<br>
+        desc: `
         <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
         <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
         <b>Working Temperature Range</b>: 32~140℉, 0~60℃
         `,
         table: ``,
+      },
+    },
+    "Tube Metric - Thread M": {
+      "Male Straight": {
+        dim: "PC-M Series.jpg",
+        eng: "Male Straight",
+        thumb: "https://cdcpneumatics.com/data/product/pc_m.png",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>∅P</th><th>T</th><th>L</th><th>A</th><th>B</th></tr></thead><tbody><tr><td>PC 06-M12</td><td>6</td><td>12.3</td><td>M12*1.5p</td><td>24.7</td><td>8</td><td>14.8</td></tr><tr><td>PC 06-M14</td><td>6</td><td>12.3</td><td>M14*1.5p</td><td>24.7</td><td>8.0</td><td>14.8</td></tr><tr><td>PC 06-M16</td><td>6</td><td>12.3</td><td>M16*1.5P</td><td>24.7</td><td>8.0</td><td>14.8</td></tr><tr><td>PC 06-M22</td><td>6</td><td>12.3</td><td>M22*1.5P</td><td>24.7</td><td>8.0</td><td>14.8</td></tr><tr><td>PC 08-M12</td><td>8</td><td>14.3</td><td>M12*1.5p</td><td>26.0</td><td>8.0</td><td>18.4</td></tr><tr><td>PC 08-M14</td><td>8</td><td>14.3</td><td>M14*1.5p</td><td>26.0</td><td>8.0</td><td>18.4</td></tr><tr><td>PC 08-M16</td><td>8</td><td>14.3</td><td>M16*1.5p</td><td>25.0</td><td>8.0</td><td>18.4</td></tr><tr><td>PC 08-M22</td><td>8</td><td>14.3</td><td>M22*1.5p</td><td>24.0</td><td>8.0</td><td>18.4</td></tr><tr><td>PC 10-M12</td><td>10</td><td>16.5</td><td>M12*1.5p</td><td>31.0</td><td>8.0</td><td>19.7</td></tr><tr><td>PC 10-M14</td><td>10</td><td>16.5</td><td>M14*1.5p</td><td>31.0</td><td>8.0</td><td>19.7</td></tr><tr><td>PC 10-M16</td><td>10</td><td>16.5</td><td>M16*1.5p</td><td>27.0</td><td>8.0</td><td>19.7</td></tr><tr><td>PC 10-M22</td><td>10</td><td>16.5</td><td>M22*1.5p</td><td>26.0</td><td>8.0</td><td>19.7</td></tr><tr><td>PC 12-M12</td><td>12</td><td>19.0</td><td>M12*1.5p</td><td>33.9</td><td>8.0</td><td>21.9</td></tr><tr><td>PC 12-M14</td><td>12</td><td>19.0</td><td>M14*1.5p</td><td>33.4</td><td>8.0</td><td>21.9</td></tr><tr><td>PC 12-M16</td><td>12</td><td>19.0</td><td>M16*1.5p</td><td>33.4</td><td>8.0</td><td>21.9</td></tr><tr><td>PC 12-M22</td><td>12</td><td>19.0</td><td>M22*1.5p</td><td>28.9</td><td>8.0</td><td>21.9</td></tr></tbody></table>`,
+      },
+    },
+    "Tube Metric - Thread G": {
+      "Male Straight": {
+        dim: "PC-G Series.jpg",
+        eng: "Male Straight",
+        thumb: "https://cdcpneumatics.com/data/product/pc_g.png",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>∅P</th><th>T</th><th>L</th><th>A</th><th>B</th></tr></thead><tbody><tr><td>PC04-G01</td><td>4</td><td>10.3</td><td>G 1/8</td><td>20.0</td><td>5</td><td>14.8</td></tr><tr><td>PC04-G02</td><td>4</td><td>10.3</td><td>G 1/4</td><td>21.5</td><td>6.5</td><td>14.8</td></tr><tr><td>PC04-G03</td><td>4</td><td>10.3</td><td>G 3/8</td><td>21.5</td><td>6.5</td><td>14.8</td></tr><tr><td>PC06-G01</td><td>6</td><td>12.3</td><td>G 1/8</td><td>21.2</td><td>5.0</td><td>16.4</td></tr><tr><td>PC06-G02</td><td>6</td><td>12.3</td><td>G 1/4</td><td>23.2</td><td>6.5</td><td>16.4</td></tr><tr><td>PC06-G03</td><td>6</td><td>12.3</td><td>G 3/8</td><td>22.2</td><td>6.5</td><td>16.4</td></tr><tr><td>PC08-G01</td><td>8</td><td>14.3</td><td>G 1/8</td><td>26.9</td><td>5.0</td><td>18.4</td></tr><tr><td>PC08-G02</td><td>8</td><td>14.3</td><td>G 1/4</td><td>24.4</td><td>6.5</td><td>18.4</td></tr><tr><td>PC08-G03</td><td>8</td><td>14.3</td><td>G 3/8</td><td>23.4</td><td>6.5</td><td>18.4</td></tr><tr><td>PC08-G04</td><td>8</td><td>14.3</td><td>G 1/2</td><td>23.9</td><td>8.0</td><td>18.4</td></tr><tr><td>PC10-G01</td><td>10</td><td>16.5</td><td>G 1/8</td><td>28.9</td><td>5.0</td><td>19.7</td></tr><tr><td>PC10-G02</td><td>10</td><td>16.5</td><td>G 1/4</td><td>28.9</td><td>6.5</td><td>19.7</td></tr><tr><td>PC10-G03</td><td>10</td><td>16.5</td><td>G 3/8</td><td>25.4</td><td>6.5</td><td>19.7</td></tr><tr><td>PC10-G04</td><td>10</td><td>16.5</td><td>G 1/2</td><td>25.9</td><td>8.0</td><td>19.7</td></tr><tr><td>PC12-G02</td><td>12</td><td>19.0</td><td>G 1/4</td><td>31.7</td><td>6.5</td><td>21.9</td></tr><tr><td>PC12-G03</td><td>12</td><td>19.0</td><td>G 3/8</td><td>27.2</td><td>6.5</td><td>21.9</td></tr><tr><td>PC12-G04</td><td>12</td><td>19.0</td><td>G 1/2</td><td>28.2</td><td>8.0</td><td>21.9</td></tr><tr><td>PC14-G03</td><td>14</td><td>21.8</td><td>G 3/8</td><td>33.5</td><td>6.5</td><td>23.8</td></tr><tr><td>PC14-G04</td><td>14</td><td>21.8</td><td>G 1/2</td><td>32.8</td><td>8.0</td><td>23.8</td></tr><tr><td>PC16-G03</td><td>16</td><td>24.0</td><td>G 3/8</td><td>36.5</td><td>8.0</td><td>24.0</td></tr><tr><td>PC16-G04</td><td>16</td><td>24.0</td><td>G 1/2</td><td>36.5</td><td>9.0</td><td>24.0</td></tr></tbody></table>`,
+      },
+      "Female Straight": {
+        dim: "PCF-G Series.jpg",
+        eng: "Female Straight",
+        thumb: "https://cdcpneumatics.com/data/product/pcf_g.png",
+        desc: `
+        <b>Applicable Fluid Type</b>: AIR (No other type of gas or liquid)<br>
+        <b>Working Pressure Range</b>: 0~150PSI, 0~9.9Kgf/cm²(0~990KPa)<br>
+        <b>Working Temperature Range</b>: 32~140℉, 0~60℃
+        `,
+        table: `<table><thead><tr><th>Model</th><th>∅D</th><th>∅P</th><th>T</th><th>L</th><th>A</th><th>B</th></tr></thead><tbody><tr><td>PCF04-G01</td><td>4</td><td>10.0</td><td>G 1/8</td><td>27.1</td><td>9</td><td>14.8</td></tr><tr><td>PCF04-G02</td><td>4</td><td>10.0</td><td>G 1/4</td><td>29.1</td><td>11.0</td><td>14.8</td></tr><tr><td>PCF04-G03</td><td>4</td><td>10.0</td><td>G 3/8</td><td>30.1</td><td>12.0</td><td>14.8</td></tr><tr><td>PCF06-G01</td><td>6</td><td>12.0</td><td>G 1/8</td><td>28.2</td><td>10.0</td><td>16.4</td></tr><tr><td>PCF06-G02</td><td>6</td><td>12.0</td><td>G 1/4</td><td>30.2</td><td>11.0</td><td>16.4</td></tr><tr><td>PCF06-G03</td><td>6</td><td>12.0</td><td>G 3/8</td><td>31.2</td><td>12.0</td><td>16.4</td></tr><tr><td>PCF08-G01</td><td>8</td><td>14.0</td><td>G 1/8</td><td>29.4</td><td>10.0</td><td>18.4</td></tr><tr><td>PCF08-G02</td><td>8</td><td>14.0</td><td>G 1/4</td><td>31.4</td><td>11.0</td><td>18.4</td></tr><tr><td>PCF08-G03</td><td>8</td><td>14.0</td><td>G 3/8</td><td>32.4</td><td>12.0</td><td>18.4</td></tr><tr><td>PCF08-G04</td><td>8</td><td>14.0</td><td>G 1/2</td><td>34.4</td><td>14.0</td><td>18.4</td></tr><tr><td>PCF10-G01</td><td>10</td><td>16.4</td><td>G 1/8</td><td>32.6</td><td>10.0</td><td>19.7</td></tr><tr><td>PCF10-G02</td><td>10</td><td>16.4</td><td>G 1/4</td><td>32.6</td><td>11.0</td><td>19.7</td></tr><tr><td>PCF10-G03</td><td>10</td><td>16.4</td><td>G 3/8</td><td>33.6</td><td>12.0</td><td>19.7</td></tr><tr><td>PCF10-G04</td><td>10</td><td>16.4</td><td>G 1/2</td><td>35.6</td><td>14.0</td><td>19.7</td></tr><tr><td>PCF12-G02</td><td>12</td><td>19.0</td><td>G 1/4</td><td>37.1</td><td>11.0</td><td>21.9</td></tr><tr><td>PCF12-G03</td><td>12</td><td>19.0</td><td>G 3/8</td><td>37.1</td><td>12.0</td><td>21.9</td></tr><tr><td>PCF12-G04</td><td>12</td><td>19.0</td><td>G 1/2</td><td>39.1</td><td>14.0</td><td>21.9</td></tr></tbody></table>`,
       },
     },
   }
