6e52f82a8ba45b2990cb374b2e31d9118dc789a9
buuchuthe <buuchuthe@gmail.com>
2026-03-15 13:32:47 +0700

    Update Hoses.html

--- assets/js/hose.js
+++ assets/js/hose.js
@@ -14,8 +14,157 @@ const productMap = {
         `,
         table: `<table><thead><tr><th>I.D x O.D</th><th>Threaded crimped fitting</th><th>Color</th><th>Temperature</th><th>W.P</th><th>B.P</th><th>Length</th><th>Weight</th></tr></thead><tbody><tr><td>8.5 x 14.5 mm (5/16’’)</td><td>13.8mm</td><td>Yellow / Green</td><td>-20°C ~ 60°C</td><td>700 PSI (50 kg/cm²)</td><td>2000 PSI (150 kg/cm²)</td><td>5, 10, 15, 20, 30, 50, 100 m</td><td>13 kg/roll</td></tr><tr><td>10 x 16 mm (13/32’’)</td><td>21mm</td><td>Yellow</td><td>-20°C ~ 60°C</td><td>700 PSI (50 kg/cm²)</td><td>2000 PSI (150 kg/cm²)</td><td>50 m</td><td>16 kg/roll</td></tr><tr><td>13 x 20 mm (1/2’’)</td><td>21mm</td><td>Yellow</td><td>-20°C ~ 60°C</td><td>570 PSI (40 kg/cm²)</td><td>1900 PSI (130 kg/cm²)</td><td>50 m</td><td>25 kg/roll</td></tr></tbody></table>`,
       },
+      "Ống hơi cao su": {
+        thumb: "https://thghose.vn/wp-content/uploads/2021/10/red-rubber-air-hoses-495x400-1.jpg",
+        eng: "Rubber air hose",
+        desc: `
+        <b>I.D</b>: Đường kính trong<br>
+        <b>O.D</b>: Đường kính ngoài<br>
+        `,
+        table: `<table><thead><tr><th>Size</th><th>I.D.</th><th>Thickness</th><th>Length</th><th>W.P.</th><th>B.P.</th><th>Weight</th></tr><tr><th>Inch</th><th>mm</th><th>mm</th><th>m/coil</th><th>Bar</th><th>Bar</th><th>kg/m</th></tr></thead><tbody><tr><td>3/16″</td><td>5</td><td>3.5</td><td>100</td><td>20</td><td>60</td><td>0.13</td></tr><tr><td>1/4″</td><td>6</td><td>3.5</td><td>100</td><td>20</td><td>60</td><td>0.17</td></tr><tr><td>5/16″</td><td>8</td><td>3.5</td><td>100</td><td>20</td><td>60</td><td>0.23</td></tr><tr><td>3/8″</td><td>10</td><td>3.5</td><td>100</td><td>20</td><td>60</td><td>0.26</td></tr><tr><td>1/2″</td><td>13</td><td>4.5</td><td>100</td><td>20</td><td>60</td><td>0.36</td></tr><tr><td>5/8″</td><td>16</td><td>4.5</td><td>50</td><td>20</td><td>60</td><td>0.53</td></tr><tr><td>3/4″</td><td>19</td><td>5</td><td>50</td><td>20</td><td>60</td><td>0.64</td></tr><tr><td>1″</td><td>25</td><td>5.5</td><td>50</td><td>20</td><td>60</td><td>0.85</td></tr><tr><td>1-1/4″</td><td>32</td><td>5.5</td><td>50</td><td>15</td><td>45</td><td>1.26</td></tr><tr><td>1-1/2″</td><td>38</td><td>6</td><td>50</td><td>15</td><td>45</td><td>1.42</td></tr><tr><td>2″</td><td>51</td><td>7</td><td>50</td><td>15</td><td>45</td><td>1.80</td></tr></tbody></table>`,
+      },
     },
-  }
+  },
+  "Van Khóa": {
+    "Van bi": {
+      "Van Bi Inox - Khóa Gạt Ren Trong": {
+        thumb: "https://file.hstatic.net/200000033050/file/van-bi-inox-2-manh-cao-cap-2_d13f2bce67e84cb9bff937fcae8e0c85_grande.jpg",
+        eng: "Ball Valve",
+        desc: `
+        <b>Vật liệu</b>: Inox 316<br>
+        <b>Môi trường làm việc</b>: Nước, dầu, khí, gas<br>
+        <b>Nhiệt độ làm việc</b>: 0-180 độ C<br>
+        <b>Áp suất làm việc</b>: Từ 0 -> 5.0MPa (50kgf/cm2G)<br>
+        <b>Kết nối</b>: lắp ren<br>
+        `,
+        table: `<table><thead><tr><th>DN - Thread size</th></tr></thead><tbody><tr><td>1/4" - Ren 13mm</td></tr><tr><td>3/8" - Ren 17mm</td></tr><tr><td>1/2" - Ren 21mm</td></tr><tr><td>3/4" - Ren 27mm</td></tr><tr><td>1" - Ren 34mm</td></tr><tr><td>1-1/4" -Ren 42mm</td></tr><tr><td>1-1/2" - Ren 49mm</td></tr><tr><td>2" - Ren 60mm</td></tr></tbody></table>`,
+      },
+    },
+  },
+  "Cổ Dê, Đai siết": {
+    "Cổ dê xiết vít": {
+      "Inox 304": {
+        thumb: "https://product.hstatic.net/200000902255/product/co_de_inox_304__2_.1_c4e1b8930cfb484ab211aa7c0dea9e3f.jpg",
+        eng: "Hose Clamps",
+        desc: ``,
+        table: `<table><thead><tr><th>Đường kính ống</th></tr></thead><tbody><tr><td>8-12mm</td></tr><tr><td>10-16mm</td></tr><tr><td>12-20mm</td></tr><tr><td>16-22mm</td></tr><tr><td>16-25mm</td></tr><tr><td>18-27mm</td></tr><tr><td>20-32mm</td></tr><tr><td>20-35mm</td></tr><tr><td>25-40mm</td></tr><tr><td>32-45mm</td></tr><tr><td>32-50mm</td></tr><tr><td>40-60mm</td></tr><tr><td>44-56mm</td></tr><tr><td>50-65mm</td></tr><tr><td>50-70mm</td></tr><tr><td>60-80mm</td></tr><tr><td>70-90mm</td></tr><tr><td>80-100mm</td></tr><tr><td>100-120mm</td></tr><tr><td>130-150mm</td></tr></tbody></table>`,
+      },
+    },
+    "Cổ Dê Bu Lông": {
+      "Bảng Inox 201": {
+        thumb: "https://product.hstatic.net/200000902255/product/co_de_bu_long__3__64623ac1b72b411893347f5df82369aa.jpg",
+        eng: "Hose Clamps",
+        desc: ``,
+        table: `<table><thead><tr><th>Đường Kính ống</th></tr></thead><tbody><tr><td>25mm</td></tr><tr><td>27mm</td></tr><tr><td>30mm</td></tr><tr><td>35mm</td></tr><tr><td>40mm</td></tr><tr><td>45mm</td></tr><tr><td>50mm</td></tr><tr><td>55mm</td></tr><tr><td>60mm</td></tr><tr><td>65mm</td></tr><tr><td>70mm</td></tr><tr><td>75mm</td></tr><tr><td>80mm</td></tr><tr><td>85mm</td></tr><tr><td>90mm</td></tr><tr><td>95mm</td></tr><tr><td>100mm</td></tr><tr><td>105mm</td></tr><tr><td>110mm</td></tr><tr><td>115mm</td></tr><tr><td>120mm</td></tr><tr><td>125mm</td></tr><tr><td>130mm</td></tr><tr><td>135mm</td></tr><tr><td>140mm</td></tr><tr><td>145mm</td></tr><tr><td>150mm</td></tr><tr><td>155mm</td></tr><tr><td>160mm</td></tr><tr><td>165mm</td></tr><tr><td>170mm</td></tr><tr><td>175mm</td></tr><tr><td>180mm</td></tr><tr><td>185mm</td></tr><tr><td>190mm</td></tr><tr><td>195mm</td></tr><tr><td>200mm</td></tr></tbody></table>`,
+      },
+    },
+    "Cổ Dê Cánh Bướm Tay Vặn Nhựa": {
+      "Inox 304": {
+        thumb: "https://product.hstatic.net/200000902255/product/co_de_tay_buom_304__2__e9f810235c20465f8017b0f4e7c9e94c.jpg",
+        eng: "Hose Clamps",
+        desc: ``,
+        table: `<table><thead><tr><th>Đường Kính ống</th></tr></thead><tbody><tr><td>10-12mm</td></tr><tr><td>10-16mm</td></tr><tr><td>12-20mm</td></tr><tr><td>16-22mm</td></tr><tr><td>16-25mm</td></tr><tr><td>18-27mm</td></tr><tr><td>20-32mm</td></tr><tr><td>20-35mm</td></tr><tr><td>25-40mm</td></tr><tr><td>32-45mm</td></tr><tr><td>32-50mm</td></tr><tr><td>40-60mm</td></tr><tr><td>44-56mm</td></tr><tr><td>50-70mm</td></tr><tr><td>60-80mm</td></tr></tbody></table>`,
+      },
+    },
+    "Cổ Dê Hai Dây": {
+      "Sắt Mạ Xi": {
+        thumb: "https://khailoi.com/wp-content/uploads/2025/01/Co-de-2-day-1-600x600.jpg",
+        eng: "Hose Clamps",
+        desc: ``,
+        table: `<table><thead><tr><th>Đường Kính Ống</th></tr></thead><tbody><tr><td>13</td></tr><tr><td>16</td></tr><tr><td>19</td></tr><tr><td>23</td></tr><tr><td>25</td></tr><tr><td>29</td></tr><tr><td>32</td></tr><tr><td>38</td></tr><tr><td>42</td></tr><tr><td>48</td></tr><tr><td>51</td></tr><tr><td>57</td></tr><tr><td>64</td></tr><tr><td>70</td></tr><tr><td>77</td></tr><tr><td>83</td></tr><tr><td>89</td></tr><tr><td>95</td></tr><tr><td>102</td></tr><tr><td>108</td></tr><tr><td>114</td></tr><tr><td>121</td></tr><tr><td>133</td></tr><tr><td>152</td></tr><tr><td>162</td></tr></tbody></table>`,
+      },
+    },
+  },
+  "Đầu Nối Ren Thau": {
+    "Cúc Nối": {
+      "Bít Thau Ren Ngoài": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_7f685fa410c7417db9ae0357d0034f57.jpg",
+        eng: "Pipe Cap",
+        desc: `Bít thau ren ngoài / Nút bít răng ngoài`,
+        table: `<table><thead><tr><th>Size Ren</th></tr></thead><tbody><tr><td>9.6mm</td></tr><tr><td>13mm</td></tr><tr><td>17mm</td></tr><tr><td>21mm</td></tr><tr><td>27mm</td></tr><tr><td>34mm</td></tr></tbody></table>`,
+      },
+      "Cúc Bít Ren Trong": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_4be3ac49a727414fb069175baa386d92.jpg",
+        eng: "Pipe Cap",
+        desc: ``,
+        table: `<table><thead><tr><th>Size Ren</th></tr></thead><tbody><tr><td>9.6mm - 1/8"</td></tr><tr><td>17mm - 3/8"</td></tr><tr><td>13mm - 1/4"</td></tr><tr><td>21mm - 1/2"</td></tr><tr><td>27mm - 3/4"</td></tr><tr><td>34mm - 1"</td></tr></tbody></table>`,
+      },
+      "Cà Rá Ren": {
+        thumb: "https://product.hstatic.net/200000902255/product/ca_ra_ren_trong_nho_ren_ngoai_lon__1__061cddbcfb454eb88d760e54bd93bbf3.png",
+        eng: "Hexagon Bushing",
+        desc: `
+        Cúc nối giảm lớn nhỏ, Nối Giảm Ren Trong Ren Ngoài Đồng Thau<br>
+        Kết nối hai chi tiết ren trong và ren ngoài lại với nhau
+        `,
+        table: `<table><thead><tr><th>Ren Ngoài x Ren Trong</th></tr></thead><tbody><tr><td>13 x 9.6</td></tr><tr><td>17 x 9.6</td></tr><tr><td>17 x 13</td></tr><tr><td>21 x 9.6</td></tr><tr><td>21 x 13</td></tr><tr><td>21 x 17</td></tr><tr><td>27 x 13</td></tr><tr><td>27 x 17</td></tr><tr><td>27 x 21</td></tr><tr><td>34 x 13</td></tr><tr><td>34 x 17</td></tr><tr><td>34 x 21</td></tr><tr><td>34 x 27</td></tr><tr><td>42 x 21</td></tr><tr><td>42 x 27</td></tr><tr><td>42 x 34</td></tr><tr><td>49 x 21</td></tr><tr><td>49 x 27</td></tr><tr><td>49 x 34</td></tr><tr><td>49 x 42</td></tr><tr><td>60 x 21</td></tr><tr><td>60 x 27</td></tr><tr><td>60 x 34</td></tr><tr><td>60 x 42</td></tr><tr><td>60 x 49</td></tr></tbody></table>`,
+      },
+      "Lơ Thu Ren": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_0eaf36ee88b14b2592e28c22fe9d371a.jpg",
+        eng: "",
+        desc: `
+        Sơ Mi ren - Nối Giảm Ren trong lớn/ ren ngoài nhỏ<br>
+        Kết nối hai chi tiết ren trong và ren ngoài lại với nhau
+        `,
+        table: `<table><thead><tr><th>Ren trong x Ren ngoài</th></tr></thead><tbody><tr><td>13 x 9.6</td></tr><tr><td>17 x 9.6</td></tr><tr><td>17 x 13</td></tr><tr><td>21 x 9.6</td></tr><tr><td>21 x 13</td></tr><tr><td>21 x 17</td></tr><tr><td>27 x 13</td></tr><tr><td>27 x 17</td></tr><tr><td>27 x 21</td></tr><tr><td>34 x 21</td></tr><tr><td>34 x 27</td></tr></tbody></table>`,
+      },
+      "Kép Đều": {
+        thumb: "https://product.hstatic.net/200000902255/product/noi_ren_ngoai__-_kep_bspt__1__6fe83521e21c416da66c164b633c3447.png",
+        eng: "",
+        desc: `
+        Kép Đều, Man Hai Đầu Ren Ngoài Thau<br>
+        Hai đầu ren ngoài được gọi là kép
+        `,
+        table: `<table><thead><tr><th>Size ren</th></tr></thead><tbody><tr><td>G 1/8 = DN6 = 9.6</td></tr><tr><td>G 1/4 = DN8 = 13</td></tr><tr><td>G 3/8 = DN10 = 17</td></tr><tr><td>G 1/2 = DN15 = 21</td></tr><tr><td>G 3/4 = DN20 = 27</td></tr><tr><td>G 1 = DN25 = 34</td></tr><tr><td>G 1 1/4 = DN32 = 42</td></tr><tr><td>G 1 1/2 = DN40 = 49</td></tr><tr><td>G 2 = DN50 = 60</td></tr></tbody></table>`,
+      },
+      "Kép Thu, Nối Giảm Ren": {
+        thumb: "https://bizweb.dktcdn.net/100/379/731/files/kep-ren-dong-thau.jpg?v=1683083684523",
+        eng: "Straight Reducers Connectors",
+        desc: `
+        Nối giảm ren thau, kép thu ren<br>
+        Hai đầu ren ngoài được gọi là kép
+        `,
+        table: `<table><thead><tr><th>2 Ren ngoài</th></tr></thead><tbody><tr><td>13x9.6</td></tr><tr><td>17x9.6</td></tr><tr><td>17x13</td></tr><tr><td>21x9.6</td></tr><tr><td>21x13</td></tr><tr><td>21x17</td></tr><tr><td>22x13</td></tr><tr><td>22x13.8</td></tr><tr><td>27x13</td></tr><tr><td>27x17</td></tr><tr><td>27x21</td></tr><tr><td>34x13</td></tr><tr><td>34x17</td></tr><tr><td>34x21</td></tr><tr><td>34x27</td></tr><tr><td>42x27</td></tr><tr><td>42x34</td></tr></tbody></table>`,
+      },
+      "Măng Sông Thau Đều": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_41283f3f6dfe441cb3056bad90184f83.jpg",
+        eng: "",
+        desc: `Nối thau 2 đầu ren trong (hay còn gọi là măng sông thau ren trong / nối răng trong)`,
+        table: `<table><thead><tr><th>2 đầu ren trong</th></tr></thead><tbody><tr><td>RT 9.6mm</td></tr><tr><td>Nối RT 13mm</td></tr><tr><td>RT 13mm / 9.6mm</td></tr><tr><td>Nối RT 17mm</td></tr><tr><td>Nối RT 17mm / 13mm</td></tr><tr><td>Nối RT 21mm</td></tr><tr><td>Nối RT 21mm / 13mm</td></tr><tr><td>Nối RT 21mm / 17mm</td></tr><tr><td>Nối RT 27mm</td></tr><tr><td>Nối RT 27mm/13mm</td></tr><tr><td>Nối RT 27mm/17mm</td></tr><tr><td>Nối RT 27mm/21mm</td></tr><tr><td>Nối RT 34mm</td></tr><tr><td>Nối RT 34mm/21mm</td></tr><tr><td>Nối RT 34mm/27mm</td></tr></tbody></table>`,
+      },
+      "Ốc Bít Thau Ren Lục Giác": {
+        thumb: "https://product.hstatic.net/200000902255/product/bit_luc_giac_chim_thau_ren_bspt__1__6d20f26279bb49fb97e2f54f5659d171.png",
+        eng: "",
+        desc: ``,
+        table: `<table><thead><tr><th>Size ren</th></tr></thead><tbody><tr><td>9.6 mm</td></tr><tr><td>13 mm</td></tr><tr><td>17 mm</td></tr><tr><td>21 mm</td></tr></tbody></table>`,
+      },
+      "Co ren trong/ Ren Ngoài 90°": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_d9523419d0a14dc7ae283342c54179df.jpg",
+        eng: "",
+        desc: `Co ren trong - ren ngoài 90° bằng đồng thau là phụ kiện dùng để kết nối và chuyển hướng`,
+        table: `<table><thead><tr><th>Size Ren</th></tr></thead><tbody><tr><td>RT 9.6mm / RN 9.6mm</td></tr><tr><td>RT 9.6mm / RN 13mm</td></tr><tr><td>RT 13mm / RN 13mm</td></tr><tr><td>RT 13mm / RN 9.6mm</td></tr><tr><td>RT 13mm / RN 17mm</td></tr><tr><td>RT 17mm / RN 9.6mm</td></tr><tr><td>RT 17mm / RN 13mm</td></tr><tr><td>RT 17mm / RN 17mm</td></tr><tr><td>RT 21mm / RN 21mm</td></tr></tbody></table>`,
+      },
+    },
+    "Nối Đuôi Chuột/Ra Ren": {
+      "Béc Ren Ngoài Có Nối Đuôi Chuột": {
+        thumb: "https://product.hstatic.net/200000902255/product/drn__4__af7121ad2a3245739e2f546e47fbbe8f.jpg",
+        eng: "",
+        desc: `Nối thau đuôi chuột`,
+        table: `<table><thead><tr><th>Size Ren - Size ống</th></tr></thead><tbody><tr><td>9.6 - đuôi 6</td></tr><tr><td>9.6 - đuôi 8</td></tr><tr><td>9.6 - đuôi 10</td></tr><tr><td>13 - đuôi 6</td></tr><tr><td>13 - đuôi 8</td></tr><tr><td>13 - đuôi 10</td></tr><tr><td>13 - đuôi 12</td></tr><tr><td>17 - đuôi 6</td></tr><tr><td>17 - đuôi 8</td></tr><tr><td>17 - đuôi 10</td></tr><tr><td>17 - đuôi 12</td></tr><tr><td>17 - đuôi 14</td></tr><tr><td>17 - đuôi 16</td></tr><tr><td>21 - đuôi 6</td></tr><tr><td>21 - đuôi 8</td></tr><tr><td>21 - đuôi 10</td></tr><tr><td>21 - đuôi 12</td></tr><tr><td>21 - đuôi 14</td></tr><tr><td>21 - đuôi 16</td></tr><tr><td>21 - đuôi 18</td></tr><tr><td>21 - đuôi 20</td></tr><tr><td>27 - đuôi 6</td></tr><tr><td>27 - đuôi 8</td></tr><tr><td>27 - đuôi 10</td></tr><tr><td>27 - đuôi 12</td></tr><tr><td>27 - đuôi 14</td></tr><tr><td>27 - đuôi 16</td></tr><tr><td>27 - đuôi 18</td></tr><tr><td>27 - đuôi 20</td></tr><tr><td>27 - đuôi 25</td></tr><tr><td>34 - đuôi 21</td></tr><tr><td>34 - đuôi 27</td></tr><tr><td>34 - đuôi 34</td></tr></tbody></table>`,
+      },
+      "Béc Ren Trong Có Nối Đuôi Chuột": {
+        thumb: "https://product.hstatic.net/200000902255/product/drt__2__73a6827e8ad64446a18eb87436e85e50.jpg",
+        eng: "",
+        desc: `Nối thau đuôi chuột`,
+        table: `<table><thead><tr><th>Size Ren - Size ống</th></tr></thead><tbody><tr><td>RT 9.6 - đuôi 6</td></tr><tr><td>RT 9.6 - đuôi 8</td></tr><tr><td>RT 9.6 - đuôi 10</td></tr><tr><td>RT 13 - đuôi 6</td></tr><tr><td>RT 13 - đuôi 8</td></tr><tr><td>RT 13 - đuôi 10</td></tr><tr><td>RT 13 - đuôi 12</td></tr><tr><td>RT 17 - đuôi 6</td></tr><tr><td>RT 17 - đuôi 8</td></tr><tr><td>RT 17 - đuôi 10</td></tr><tr><td>RT 17 - đuôi 12</td></tr><tr><td>RT 17 - đuôi 14</td></tr><tr><td>RT 17 - đuôi 16</td></tr><tr><td>RT 21 - đuôi 6</td></tr><tr><td>RT 21 - đuôi 8</td></tr><tr><td>RT 21 - đuôi 10</td></tr><tr><td>RT 21 - đuôi 12</td></tr><tr><td>RT 21 - đuôi 14</td></tr><tr><td>RT 21 - đuôi 16</td></tr><tr><td>RT 21 - đuôi 18</td></tr><tr><td>RT 21 - đuôi 20</td></tr><tr><td>RT 27 - đuôi 8</td></tr><tr><td>RT 27 - đuôi 10</td></tr><tr><td>RT 27 - đuôi 12</td></tr><tr><td>RT 27 - đuôi 14</td></tr><tr><td>RT 27 - đuôi 16</td></tr><tr><td>RT 27 - đuôi 18</td></tr><tr><td>RT 27 - đuôi 20</td></tr></tbody></table>`,
+      },
+      "Nối thẳng đuôi chuột": {
+        thumb: "https://product.hstatic.net/200000902255/product/upload_32a2a22ebe164c9a9f0d8b93abb1fdd3.jpg",
+        eng: "",
+        desc: ``,
+        table: `<table><thead><tr><th>Size ống</th></tr></thead><tbody><tr><td>6 mm</td></tr><tr><td>8 mm</td></tr><tr><td>10 mm</td></tr><tr><td>12 mm</td></tr><tr><td>14 mm</td></tr><tr><td>16 mm</td></tr><tr><td>18 mm</td></tr><tr><td>20 mm</td></tr></tbody></table>`,
+      },
+    },
+  },
 };
 
 function updateSelect() {
